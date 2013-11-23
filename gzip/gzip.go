871759bf33ee6e9e95f3ed676af9bc3bec352bf7
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-11-23 08:20:37 -0800

    Improved stability of gzip compression. Detecting content type

--- gzip/gzip.go
+++ gzip/gzip.go
@@ -7,22 +7,49 @@ import (
 	"strings"
 )
 
+const (
+	HeaderAcceptEncoding  = "Accept-Encoding"
+	HeaderContentEncoding = "Content-Encoding"
+	HeaderContentLength   = "Content-Length"
+	HeaderContentType     = "Content-Type"
+	HeaderVary            = "Vary"
+)
+
 var serveGzip = func(w http.ResponseWriter, r *http.Request, c martini.Context) {
 	if !strings.Contains(r.Header.Get(HeaderAcceptEncoding), "gzip") {
 		return
 	}
+
 	headers := w.Header()
 	headers.Set(HeaderContentEncoding, "gzip")
 	headers.Set(HeaderVary, HeaderAcceptEncoding)
+
 	gz := gzip.NewWriter(w)
 	defer gz.Close()
-	mw := martini.NewResponseWriter(w)
-	gzw := gzipResponseWriter{gz, mw}
-	c.MapTo(gzw, (*martini.ResponseWriter)(nil))
+
+	gzw := gzipResponseWriter{gz, martini.NewResponseWriter(w)}
+	c.MapTo(gzw, (*http.ResponseWriter)(nil))
+
 	c.Next()
+
+	// delete content length after we know we have been written to
+	gzw.Header().Del("Content-Length")
 }
 
 // All returns a Handler that adds gzip compression to all requests
 func All() martini.Handler {
 	return serveGzip
+}
+
+type gzipResponseWriter struct {
+	w *gzip.Writer
+	martini.ResponseWriter
+}
+
+func (grw gzipResponseWriter) Write(p []byte) (int, error) {
+	if len(grw.Header().Get(HeaderContentType)) == 0 {
+		grw.Header().Set(HeaderContentType, http.DetectContentType(p))
+	}
+
+	return grw.w.Write(p)
 }
