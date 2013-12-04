e96ba80908eec6804b3d0c2720f541e539ffb235
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-12-03 17:42:00 -0800

    Godoc documentation

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,3 +1,22 @@
+// Package sessions contains middleware for easy session management in Martini.
+//
+//  package main
+//
+//  import (
+//    "github.com/codegangsta/martini"
+//    "github.com/codegangsta/martini-contrib/render"
+//  )
+//
+//  func main() {
+// 	  m := martini.Classic()
+//
+// 	  store := NewCookieStore([]byte("secret123"))
+// 	  m.Use(Sessions("my_session", store))
+//
+// 	  m.Get("/", func(session Session) string {
+// 		  session.Set("hello", "world")
+// 	  })
+//  }
 package sessions
 
 import (
@@ -11,21 +30,44 @@ const (
 	errorFormat = "[sessions] ERROR! %s\n"
 )
 
+// Store is an interface for custom session stores.
 type Store interface {
 	sessions.Store
 }
 
+// NewCookieStore returns a new CookieStore.
+//
+// Keys are defined in pairs to allow key rotation, but the common case is to set a single
+// authentication key and optionally an encryption key.
+//
+// The first key in a pair is used for authentication and the second for encryption. The
+// encryption key can be set to nil or omitted in the last pair, but the authentication key
+// is required in all pairs.
+//
+// It is recommended to use an authentication key with 32 or 64 bytes. The encryption key,
+// if set, must be either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256 modes.
 func NewCookieStore(keyPairs ...[]byte) Store {
 	return sessions.NewCookieStore(keyPairs...)
 }
 
+// Session stores the values and optional configuration for a session.
 type Session interface {
-	Get(interface{}) interface{}
-	Set(interface{}, interface{})
+	// Get returns the session value associated to the given key.
+	Get(key interface{}) interface{}
+	// Set sets the session value associated to the given key.
+	Set(key interface{}, val interface{})
+	// AddFlash adds a flash message to the session.
+	// A single variadic argument is accepted, and it is optional: it defines the flash key.
+	// If not defined "_flash" is used by default.
 	AddFlash(value interface{}, vars ...string)
+	// Flashes returns a slice of flash messages from the session.
+	// A single variadic argument is accepted, and it is optional: it defines the flash key.
+	// If not defined "_flash" is used by default.
 	Flashes(vars ...string) []interface{}
 }
 
+// Sessions is a Middleware that maps a session.Session service into the Martini handler chain.
+// Sessions can use a number of storage solutions with the given store.
 func Sessions(name string, store Store) martini.Handler {
 	return func(res http.ResponseWriter, r *http.Request, c martini.Context, l *log.Logger) {
 		s, err := store.Get(r, name)
