9073991d3c32d0ada6103bee423a46b922effb0f
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-12-04 19:48:05 -0800

    JMS #38: Some major optimizations for the sessions middleware.
    Sessions is still in a weird state. I will get it cleaned up and
    have more tests. So far it is looking far better than it was

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -60,45 +60,59 @@ type Session interface {
 	// AddFlash adds a flash message to the session.
 	// A single variadic argument is accepted, and it is optional: it defines the flash key.
 	// If not defined "_flash" is used by default.
-	AddFlash(value interface{}, vars ...string)
+	//AddFlash(value interface{}, vars ...string)
 	// Flashes returns a slice of flash messages from the session.
 	// A single variadic argument is accepted, and it is optional: it defines the flash key.
 	// If not defined "_flash" is used by default.
-	Flashes(vars ...string) []interface{}
+	//Flashes(vars ...string) []interface{}
 }
 
 // Sessions is a Middleware that maps a session.Session service into the Martini handler chain.
 // Sessions can use a number of storage solutions with the given store.
 func Sessions(name string, store Store) martini.Handler {
 	return func(res http.ResponseWriter, r *http.Request, c martini.Context, l *log.Logger) {
-		s, err := store.Get(r, name)
+		// Map to the Session interface
+    s := &session{name, r, l, store, nil, false}
+		c.MapTo(s, (*Session)(nil))
+
 		// clear the context, we don't need to use
 		// gorilla context and we don't want memory leaks
 		defer context.Clear(r)
 
-		check(err, l)
-
-		// Map to the Session interface
-		c.MapTo(&session{s}, (*Session)(nil))
-
 		// Use before hook to save out the session
 		rw := res.(martini.ResponseWriter)
 		rw.Before(func(martini.ResponseWriter) {
-			check(s.Save(r, res), l)
+      if s.Written {
+        check(s.Session.Save(r, res), l)
+      }
 		})
 	}
 }
 
 type session struct {
-	*sessions.Session
+  Name string
+  Request *http.Request
+  Logger *log.Logger
+  Store Store
+  Session *sessions.Session
+  Written bool
 }
 
 func (s *session) Get(key interface{}) interface{} {
+  s.EnsureSession()
 	return s.Session.Values[key]
 }
 
 func (s *session) Set(key interface{}, val interface{}) {
+  s.EnsureSession()
+  s.Written = true
 	s.Session.Values[key] = val
+}
+
+func (s *session) EnsureSession() {
+  if s.Session == nil {
+    s.Session, _ = s.Store.Get(s.Request, s.Name)
+  }
 }
 
 func check(err error, l *log.Logger) {
