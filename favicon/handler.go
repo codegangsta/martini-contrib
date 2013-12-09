ce18098a3310b23fdff52e3c0754d26c2b44d4d5
Yann Malet <yann.malet@gmail.com>
2013-12-10 00:31:53 +0100

    Use http.ServeContent instead of http.ServeFile. Read the Favicon only when the server start.

--- favicon/handler.go
+++ favicon/handler.go
@@ -5,15 +5,30 @@ import (
 	"github.com/codegangsta/martini"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 // Creates a new handler that returns the favicon specified in `file` and set the `cache-control header`
-func Handler(file string, maxAge int) martini.Handler {
+func Handler(name string, maxAge int) martini.Handler {
+	directory, file := filepath.Split(name)
+	dir := http.Dir(directory)
+	favicon, err := dir.Open(file)
+	if err != nil {
+		// TODO (yml): Should I swallow this error
+		log.Fatal("An error occured while trying to open the favicon", err)
+	}
+
+	fstat, err := favicon.Stat()
+	if err != nil {
+		// TODO (yml): Should I swallow this error
+		log.Fatal("An error occured while accessing the file stats for the favicon", err)
+	}
+
 	return func(w http.ResponseWriter, r *http.Request, log *log.Logger) {
 		if r.URL.Path == "/favicon.ico" {
 			log.Println("[favicon] Serving ")
 			w.Header().Set("Cache-Control", fmt.Sprintf("public, max-age=%d", maxAge))
-			http.ServeFile(w, r, file)
+			http.ServeContent(w, r, fstat.Name(), fstat.ModTime(), favicon)
 		}
 	}
 }
