37fc282060a820fc07e7b7ad8647001cdb28a2a2
Daniel Hobe <hobe@gmail.com>
2013-12-25 12:03:55 -0800

    Add option to output formatted JSON using MarshallIndent

--- render/render.go
+++ render/render.go
@@ -24,67 +24,69 @@
 package render
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"github.com/codegangsta/martini"
-	"html/template"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "github.com/codegangsta/martini"
+  "html/template"
+  "io"
+  "io/ioutil"
+  "net/http"
+  "os"
+  "path/filepath"
+  "strconv"
 )
 
 const (
-	ContentType    = "Content-Type"
-	ContentLength  = "Content-Length"
-	ContentJSON    = "application/json"
-	ContentHTML    = "text/html"
-	defaultCharset = "UTF-8"
+  ContentType    = "Content-Type"
+  ContentLength  = "Content-Length"
+  ContentJSON    = "application/json"
+  ContentHTML    = "text/html"
+  defaultCharset = "UTF-8"
 )
 
 // Included helper functions for use when rendering html
 var helperFuncs = template.FuncMap{
-	"yield": func() (string, error) {
-		return "", fmt.Errorf("yield called with no layout defined")
-	},
+  "yield": func() (string, error) {
+    return "", fmt.Errorf("yield called with no layout defined")
+  },
 }
 
 // Render is a service that can be injected into a Martini handler. Render provides functions for easily writing JSON and
 // HTML templates out to a http Response.
 type Render interface {
-	// JSON writes the given status and JSON serialized version of the given value to the http.ResponseWriter.
-	JSON(status int, v interface{})
-	// HTML renders a html template specified by the name and writes the result and given status to the http.ResponseWriter.
-	HTML(status int, name string, v interface{})
-	// Error is a convenience function that writes an http status to the http.ResponseWriter.
-	Error(status int)
+  // JSON writes the given status and JSON serialized version of the given value to the http.ResponseWriter.
+  JSON(status int, v interface{})
+  // HTML renders a html template specified by the name and writes the result and given status to the http.ResponseWriter.
+  HTML(status int, name string, v interface{})
+  // Error is a convenience function that writes an http status to the http.ResponseWriter.
+  Error(status int)
 }
 
 // Delims represents a set of Left and Right delimiters for HTML template rendering
 type Delims struct {
-	// Left delimiter, defaults to {{
-	Left string
-	// Right delimiter, defaults to }}
-	Right string
+  // Left delimiter, defaults to {{
+  Left string
+  // Right delimiter, defaults to }}
+  Right string
 }
 
 // Options is a struct for specifying configuration options for the render.Renderer middleware
 type Options struct {
-	// Directory to load templates. Default is "templates"
-	Directory string
-	// Layout template name. Will not render a layout if "". Defaults to "".
-	Layout string
-	// Extensions to parse template files from. Defaults to [".tmpl"]
-	Extensions []string
-	// Funcs is a slice of FuncMaps to apply to the template upon compilation. This is useful for helper functions. Defaults to [].
-	Funcs []template.FuncMap
-	// Delims sets the action delimiters to the specified strings in the Delims struct.
-	Delims Delims
-	// Appends the given charset to the Content-Type header. Default is "UTF-8".
-	Charset string
+  // Directory to load templates. Default is "templates"
+  Directory string
+  // Layout template name. Will not render a layout if "". Defaults to "".
+  Layout string
+  // Extensions to parse template files from. Defaults to [".tmpl"]
+  Extensions []string
+  // Funcs is a slice of FuncMaps to apply to the template upon compilation. This is useful for helper functions. Defaults to [].
+  Funcs []template.FuncMap
+  // Delims sets the action delimiters to the specified strings in the Delims struct.
+  Delims Delims
+  // Appends the given charset to the Content-Type header. Default is "UTF-8".
+  Charset string
+  // Outputs human readable JSON
+  IndentJson bool
 }
 
 // Renderer is a Middleware that maps a render.Render service into the Martini handler chain. An single variadic render.Options
@@ -94,143 +96,149 @@ type Options struct {
 // If MARTINI_ENV is set to "" or "development" then templates will be recompiled on every request. For more performance, set the
 // MARTINI_ENV environment variable to "production"
 func Renderer(options ...Options) martini.Handler {
-	opt := prepareOptions(options)
-	cs := prepareCharset(opt.Charset)
-	t := compile(opt)
-	return func(res http.ResponseWriter, c martini.Context) {
-		// recompile for easy development
-		if martini.Env == martini.Dev {
-			t = compile(opt)
-		}
-		tc, _ := t.Clone()
-		c.MapTo(&renderer{res, tc, opt, cs}, (*Render)(nil))
-	}
+  opt := prepareOptions(options)
+  cs := prepareCharset(opt.Charset)
+  t := compile(opt)
+  return func(res http.ResponseWriter, c martini.Context) {
+    // recompile for easy development
+    if martini.Env == martini.Dev {
+      t = compile(opt)
+    }
+    tc, _ := t.Clone()
+    c.MapTo(&renderer{res, tc, opt, cs}, (*Render)(nil))
+  }
 }
 
 func prepareCharset(charset string) string {
-	if len(charset) != 0 {
-		return "; charset=" + charset
-	}
+  if len(charset) != 0 {
+    return "; charset=" + charset
+  }
 
-	return "; charset=" + defaultCharset
+  return "; charset=" + defaultCharset
 }
 
 func prepareOptions(options []Options) Options {
-	var opt Options
-	if len(options) > 0 {
-		opt = options[0]
-	}
+  var opt Options
+  if len(options) > 0 {
+    opt = options[0]
+  }
 
-	// Defaults
-	if len(opt.Directory) == 0 {
-		opt.Directory = "templates"
-	}
-	if len(opt.Extensions) == 0 {
-		opt.Extensions = []string{".tmpl"}
-	}
+  // Defaults
+  if len(opt.Directory) == 0 {
+    opt.Directory = "templates"
+  }
+  if len(opt.Extensions) == 0 {
+    opt.Extensions = []string{".tmpl"}
+  }
 
-	return opt
+  return opt
 }
 
 func compile(options Options) *template.Template {
-	dir := options.Directory
-	t := template.New(dir)
-	t.Delims(options.Delims.Left, options.Delims.Right)
-	// parse an initial template in case we don't have any
-	template.Must(t.Parse("Martini"))
+  dir := options.Directory
+  t := template.New(dir)
+  t.Delims(options.Delims.Left, options.Delims.Right)
+  // parse an initial template in case we don't have any
+  template.Must(t.Parse("Martini"))
 
-	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		r, err := filepath.Rel(dir, path)
-		if err != nil {
-			return err
-		}
+  filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+    r, err := filepath.Rel(dir, path)
+    if err != nil {
+      return err
+    }
 
-		ext := filepath.Ext(r)
-		for _, extension := range options.Extensions {
-			if ext == extension {
+    ext := filepath.Ext(r)
+    for _, extension := range options.Extensions {
+      if ext == extension {
 
-				buf, err := ioutil.ReadFile(path)
-				if err != nil {
-					panic(err)
-				}
+        buf, err := ioutil.ReadFile(path)
+        if err != nil {
+          panic(err)
+        }
 
-				name := (r[0 : len(r)-len(ext)])
-				tmpl := t.New(filepath.ToSlash(name))
+        name := (r[0 : len(r)-len(ext)])
+        tmpl := t.New(filepath.ToSlash(name))
 
-				// add our funcmaps
-				for _, funcs := range options.Funcs {
-					tmpl.Funcs(funcs)
-				}
+        // add our funcmaps
+        for _, funcs := range options.Funcs {
+          tmpl.Funcs(funcs)
+        }
 
-				// Bomb out if parse fails. We don't want any silent server starts.
-				template.Must(tmpl.Funcs(helperFuncs).Parse(string(buf)))
-				break
-			}
-		}
+        // Bomb out if parse fails. We don't want any silent server starts.
+        template.Must(tmpl.Funcs(helperFuncs).Parse(string(buf)))
+        break
+      }
+    }
 
-		return nil
-	})
+    return nil
+  })
 
-	return t
+  return t
 }
 
 type renderer struct {
-	http.ResponseWriter
-	t               *template.Template
-	opt             Options
-	compiledCharset string
+  http.ResponseWriter
+  t               *template.Template
+  opt             Options
+  compiledCharset string
 }
 
 func (r *renderer) JSON(status int, v interface{}) {
-	result, err := json.Marshal(v)
-	if err != nil {
-		http.Error(r, err.Error(), 500)
-		return
-	}
+  var result []byte
+  var err error
+  if r.opt.IndentJson {
+    result, err = json.MarshalIndent(v, "", "  ")
+  } else {
+    result, err = json.Marshal(v)
+  }
+  if err != nil {
+    http.Error(r, err.Error(), 500)
+    return
+  }
 
-	// json rendered fine, write out the result
-	r.Header().Set(ContentType, ContentJSON+r.compiledCharset)
-	r.WriteHeader(status)
-	r.Write(result)
+  // json rendered fine, write out the result
+  r.Header().Set(ContentType, ContentJSON+r.compiledCharset)
+  r.WriteHeader(status)
+  r.Write(result)
 }
 
 func (r *renderer) HTML(status int, name string, binding interface{}) {
-	// assign a layout if there is one
-	if len(r.opt.Layout) > 0 {
-		r.addYield(name, binding)
-		name = r.opt.Layout
-	}
+  // assign a layout if there is one
+  if len(r.opt.Layout) > 0 {
+    r.addYield(name, binding)
+    name = r.opt.Layout
+  }
 
-	out, err := r.execute(name, binding)
-	if err != nil {
-		http.Error(r, err.Error(), http.StatusInternalServerError)
-		return
-	}
+  out, err := r.execute(name, binding)
+  if err != nil {
+    http.Error(r, err.Error(), http.StatusInternalServerError)
+    return
+  }
 
-	// template rendered fine, write out the result
-	r.Header().Set(ContentType, ContentHTML+r.compiledCharset)
-	r.Header().Set(ContentLength, strconv.Itoa(out.Len()))
-	r.WriteHeader(status)
-	io.Copy(r, out)
+  // template rendered fine, write out the result
+  r.Header().Set(ContentType, ContentHTML+r.compiledCharset)
+  r.Header().Set(ContentLength, strconv.Itoa(out.Len()))
+  r.WriteHeader(status)
+  io.Copy(r, out)
 }
 
 // Error writes the given HTTP status to the current ResponseWriter
 func (r *renderer) Error(status int) {
-	r.WriteHeader(status)
+  r.WriteHeader(status)
 }
 
 func (r *renderer) execute(name string, binding interface{}) (*bytes.Buffer, error) {
-	buf := new(bytes.Buffer)
-	return buf, r.t.ExecuteTemplate(buf, name, binding)
+  buf := new(bytes.Buffer)
+  return buf, r.t.ExecuteTemplate(buf, name, binding)
 }
 
 func (r *renderer) addYield(name string, binding interface{}) {
-	funcs := template.FuncMap{
-		"yield": func() (template.HTML, error) {
-			buf, err := r.execute(name, binding)
-			// return safe html here since we are rendering our own template
-			return template.HTML(buf.String()), err
-		},
-	}
-	r.t.Funcs(funcs)
+  funcs := template.FuncMap{
+    "yield": func() (template.HTML, error) {
+      buf, err := r.execute(name, binding)
+      // return safe html here since we are rendering our own template
+      return template.HTML(buf.String()), err
+    },
+  }
+  r.t.Funcs(funcs)
 }
